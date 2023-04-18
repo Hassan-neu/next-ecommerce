33ba4cd1015199239e2630429edd714d514009eb
Hassan Adeyemi <hassanadeyemi01@gmail.com>
2023-04-18 15:45:13 +0100

    hero images fixed
    changed hero image to background with little animation

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,27 +1,20 @@
 import React from "react";
 import { urlFor } from "@/lib/client";
 import Image from "next/image";
-const Hero = ({ products }) => {
-    const newProduct = products.slice(0, 7);
-    console.log(newProduct);
+const Hero = () => {
     return (
         <div className="hero_wrapper">
             <div className="hero_head">
                 <h1>Step into greatness with our football boots</h1>
             </div>
             <div className="hero_img-container">
-                {newProduct?.map((product) => {
-                    return (
-                        <div className="hero-img" key={product._id}>
-                            <Image
-                                src={urlFor(product.image[0]).url()}
-                                width={300}
-                                height={300}
-                                alt={product.name}
-                            />
-                        </div>
-                    );
-                })}
+                <div className="hero-img"></div>
+                <div className="hero-img"></div>
+                <div className="hero-img"></div>
+                <div className="hero-img"></div>
+                <div className="hero-img"></div>
+                <div className="hero-img"></div>
+                <div className="hero-img"></div>
             </div>
         </div>
     );
