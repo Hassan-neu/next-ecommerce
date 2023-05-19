9ad05ab0cf7063b236d33f4d48626e853212d7d9
Hassan Adeyemi <hassanadeyemi01@gmail.com>
2023-05-19 21:30:44 +0100

    product navigation from home now fixed to image only

--- components/productCard.jsx
+++ components/productCard.jsx
@@ -7,33 +7,33 @@ const ProductCard = ({ prod }) => {
     const { name, price, description, image, brand, slug, _id } = prod;
     const { addToCart } = useStateContext();
     return (
-        <Link href={`/product/${slug.current}`}>
-            <div className="product_card">
-                <div className="product-img">
+        <div className="product_card">
+            <div className="product-img">
+                <Link href={`/product/${slug.current}`}>
                     <Image
                         src={urlFor(image && image[0]).url()}
                         width={200}
                         height={150}
                         alt={name}
                     />
-                </div>
-                <div className="product-head">
-                    <h4>{name}</h4>
-                </div>
-                <div className="product-price">
-                    <span>${price}</span>
-                </div>
-                <div className="product-brand">{brand}</div>
-                <div className="product-btn">
-                    <button
-                        type="button"
-                        onClick={() => addToCart(name, image, 1, _id, price)}
-                    >
-                        Add to cart
-                    </button>
-                </div>
+                </Link>
             </div>
-        </Link>
+            <div className="product-head">
+                <h4>{name}</h4>
+            </div>
+            <div className="product-price">
+                <span>${price}</span>
+            </div>
+            <div className="product-brand">{brand}</div>
+            <div className="product-btn">
+                <button
+                    type="button"
+                    onClick={() => addToCart(name, image, 1, _id, price)}
+                >
+                    Add to cart
+                </button>
+            </div>
+        </div>
     );
 };
 
